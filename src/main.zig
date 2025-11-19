a3b1c240fff3dfe861ff7223757da09634e32cd7
Taylor Plewe <tplewe@outlook.com>
2025-11-18 20:41:30 -0700

    start work on creating Windows window

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,14 @@ const c = @cImport({
     @cInclude("windows.h");
 });
 
+export fn mainWindowCallback(window: win.HWND, msg: win.UINT, w_param: win.WPARAM, l_param: win.LPARAM) win.LRESULT {
+    _ = window;
+    _ = msg;
+    _ = w_param;
+    _ = l_param;
+    return 0;
+}
+
 pub export fn wWinMain(hInstance: win.HINSTANCE, hPrevInstance: ?win.HINSTANCE, lpCmdLine: ?[*:0]u16, nShowCmd: c_int) c_int {
     // c.CreateWindowExA(dwExStyle: c_ulong,
     //   lpClassName: [*c]const u8,
@@ -19,7 +27,12 @@ pub export fn wWinMain(hInstance: win.HINSTANCE, hPrevInstance: ?win.HINSTANCE, 
     //   hInstance: [*c]struct_HINSTANCE__,
     //   lpParam: ?*anyopaque)
 
-    _ = c.MessageBoxA(null, "ayooooo", "noooooo", c.MB_OK);
+    const window_class: c.WNDCLASS = .{
+        .style = c.CS_OWNDC | c.CS_HREDRAW | c.CS_VREDRAW,
+        .lpfnWndProc = mainWindowCallback,
+        .hInstance = hInstance,
+        .lpszClassName = "AyooooTestclassname",
+    };
 
     _ = hInstance;
     _ = hPrevInstance;
